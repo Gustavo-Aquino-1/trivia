41523f0b54e2033e68d50e42cef9c75ee3a95486
Gustavo-Aquino-1 <guhaquino10@gmail.com>
2022-10-11 15:03:23 -0300

    requisito 12 e 13

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,11 +1,47 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import Header from '../components/Header';
 
 class Feedback extends Component {
+  state = {
+    message: '',
+  };
+
+  componentDidMount() {
+    this.messageStatus();
+  }
+
+  messageStatus = () => {
+    const { assertions } = this.props;
+    const min = 3;
+    if (assertions < min) {
+      this.setState({ message: 'Could be better...' });
+    } else {
+      this.setState({ message: 'Well Done!' });
+    }
+  };
+
   render() {
+    const { message } = this.state;
     return (
-      <div data-testid="feedback-text">Página de Feedback</div>
+      <>
+        <Header />
+        <div>
+          <p data-testid="feedback-text">Página de Feedback</p>
+          <h3 data-testid="feedback-text">{message}</h3>
+        </div>
+      </>
     );
   }
 }
 
-export default Feedback;
+const mapStateToProps = ({ player }) => ({
+  assertions: player.assertions,
+});
+
+Feedback.propTypes = {
+  assertions: PropTypes.number.isRequired,
+};
+
+export default connect(mapStateToProps)(Feedback);
